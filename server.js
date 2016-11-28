2fe7a1bd45d2a47703e1e09409c2404f0f0d6836
William Werner <bill_werner@homedepot.com>
2016-11-28 17:25:15 -0500

    new post todos

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+app.use(bodyParser.json());
 
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false 
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Play with dog',
-    completed: true
-}];
+// var todos = [{
+//     id: 1,
+//     description: 'Meet mom for lunch',
+//     completed: false 
+// }, {
+//     id: 2,
+//     description: 'Go to market yo',
+//     completed: false
+// }, {
+//     id: 3,
+//     description: 'Play with dog',
+//     completed: true
+// }];
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
@@ -39,6 +44,16 @@ app.get('/todos/:id', function(req,res){
     } else{
         res.status(404).send();
     }
+});
+
+app.post('/todos', function(req,res){
+    var body = req.body;
+
+    body.id = todoNextId++;
+
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, function(){
