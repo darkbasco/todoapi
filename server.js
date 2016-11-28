2c96723b67af182151a6994d5a3444e18f47590b
William Werner <bill_werner@homedepot.com>
2016-11-28 17:42:56 -0500

    validation

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
-app.use(bodyParser.json());
 
-// var todos = [{
-//     id: 1,
-//     description: 'Meet mom for lunch',
-//     completed: false 
-// }, {
-//     id: 2,
-//     description: 'Go to market yo',
-//     completed: false
-// }, {
-//     id: 3,
-//     description: 'Play with dog',
-//     completed: true
-// }];
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     res.send('Todo API Root');
@@ -31,13 +19,7 @@ app.get('/todos', function(req,res){
 
 app.get('/todos/:id', function(req,res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-
-    todos.forEach(function(todo) {
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }    
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -48,6 +30,10 @@ app.get('/todos/:id', function(req,res){
 
 app.post('/todos', function(req,res){
     var body = req.body;
+
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
 
     body.id = todoNextId++;
 
